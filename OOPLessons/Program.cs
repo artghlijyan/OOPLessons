3b557c396a2e124c05303d2e9a9fe1b09d1a4f10
Arthur Ghlijyan <deckuser@hotmail.com>
2020-08-02 15:21:36 +0400

    Add project files.

---
+++ OOPLessons/Program.cs
@@ -0,0 +1,13 @@
+﻿using OOPLessons.Test.Trash;
+using System;
+
+namespace OOPLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrashMain.OldMain();
+        }
+    }
+}
---
+++ OOPLessons/Test/Trash/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLessons.Test.Trash
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() : this("UnKnown")
+        {
+
+        }
+
+        public Person(string name1) : this(name1, 18)
+        {
+
+        }
+
+        public Person(string name1, int age1)
+        {
+            this.name = name1;
+            this.age = age1;
+        }
+
+        private void GetInfo()
+        {
+            Console.WriteLine($"Name: {name}  Age: {age}");
+        }
+
+        public override string ToString()
+        {
+            this.GetInfo();
+            return base.ToString();
+        }
+    }
+}
